ed6315f418780dba914aace71ed9dd810d650b32
ElvisCen <2468019104@qq.com>
2020-05-18 11:10:34 +0800

    删除数组中的重复项

---
+++ Week_01/Week_01_Homework/Week_01_Homework/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Week_01_Homework
+//
+//  Created by weclouds on 2020/5/18.
+//  Copyright © 2020 weclouds. All rights reserved.
+//
+
+import Foundation
+
+let removeDup = Solution26()
+var nums1 = [1,1,2]
+let res1 = removeDup.removeDuplicates(&nums1)
+print("result1 - \(res1)")
+var nums2 = [0,0,1,1,1,2,2,3,3,4]
+let res2 = removeDup.removeDuplicates(&nums2)
+print("result2 - \(res2)")
