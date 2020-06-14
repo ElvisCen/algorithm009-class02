759612527d98009a98eaeffe6bb588b7211bca7b
ElvisCen <2468019104@qq.com>
2020-06-14 08:10:32 +0800

    第四周作业--工程创建

---
+++ Week_04/Week_04_homework/Week_04_homework/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Week_04_homework
+//
+//  Created by weclouds on 2020/6/14.
+//  Copyright © 2020 weclouds. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
